497b07792c53f6909f6b1aa9ae5131b0e96a6cc1
AidanP71 <marineforce171@gmail.com>
2025-03-20 09:14:38 -0400

    done with html and css. NEED HELP WITH JAVASCRIPT!

---
+++ script.js
@@ -0,0 +1,18 @@
+const button1 = document.querySelector
+const button2 = document.querySelector
+const button3 = document.querySelector
+
+let yscore = 0;
+let cscore = 0;
+
+button_container. array.forEach(element => {
+    button_container.addEventListener ("click", () => {
+        const button1 , button2 , button3 = button.id;
+        playgame (user button1, button2, button3) 
+    });
+});
+
+function playgame button1, button2, button3
+
+const ycopt = yscore.id
+playgame userchoice ()
